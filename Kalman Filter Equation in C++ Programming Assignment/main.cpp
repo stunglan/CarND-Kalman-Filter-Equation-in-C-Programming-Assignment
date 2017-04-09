27a24b4c08b429300a3f90d9b707a6196f9a4c33
knut tungland <stunglan@gmail.com>
2017-04-09 10:15:15 +0200

    Initial Commit

---
+++ Kalman Filter Equation in C++ Programming Assignment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Kalman Filter Equation in C++ Programming Assignment
+//
+//  Created by knut tungland on 09/04/2017.
+//  Copyright © 2017 knut tungland. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
